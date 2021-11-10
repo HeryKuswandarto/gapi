4f42f7ce804348c576d44d8f9d46199b98b1fecb
Martanto <martanto@live.com>
2021-11-10 17:03:16 +0700

    Styling form create data gunung api

--- resources/views/gunungapi/create.blade.php
+++ resources/views/gunungapi/create.blade.php
@@ -3,17 +3,61 @@
 @section('judul_halaman', 'Gunungapi - Add New Data')
 
 @section('konten')
-<a href="{{route('gunungapi.index')}}">Back</a>
-<br/>
-<form action="{{ route('gunungapi.store') }}" method="post">
-   {{csrf_field()}}
-   Kode<br/><input type="text" name="code" required="required"><br/>
-   Nama Gunungapi<br/><input type="text" name="name" required="required"><br/>
-   Nama Lain<br/><input type="text" name="othername" required="required"><br/>
-   Latitude/Lintang<br/><input type="number" name="latitude" required="required"><br/>
-   Longitude/Bujur<br/><input type="number" name="longitude" required="required"><br/>
-   Altitude<br/><input type="number" name="altitude" required="required"><br/>
-   Region/Propinsi<br/><input type="text" name="region" required="required"><br/><br/>
-   <input type="submit" value ="Save">
-</form>
+<div class="row">
+    <div class="col">
+        <a href="{{route('gunungapi.index')}}">Back</a>
+    </div>
+</div>
+
+<div class="row mt-5">
+    <div class="col-md-6">
+        <form action="{{ route('gunungapi.store') }}" method="post">
+            {{csrf_field()}}
+            <div class="form-group">
+                <label for="kode">Kode Gunung Api</label>
+                <input name="code" type="text" class="form-control" required>
+                <small class="form-text text-muted">Masukkan kode gunung api.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="name">Nama Gunung Api</label>
+                <input name="name" type="text" class="form-control" required>
+                <small class="form-text text-muted">Masukkan nama gunung api.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="othername">Nama Lain Gunung Api</label>
+                <input name="othername" type="text" class="form-control" required>
+                <small class="form-text text-muted">Masukkan alias atau nama lain gunung api.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="latitude">Latitude/Lintang</label>
+                <input name="latitude" type="number" class="form-control" required>
+                <small class="form-text text-muted">Koordinat latitude/lintang gunung api.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="longitude">Latitude/Bujur</label>
+                <input name="longitude" type="number" class="form-control" required>
+                <small class="form-text text-muted">Koordinat longitude/bujur gunung api.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="altitude">Altitude/Ketinggian (dalam meter)</label>
+                <input name="altitude" type="number" class="form-control" required>
+                <small class="form-text text-muted">Altitude atau ketinggian gunung api dalam mdpl.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="region">Region/Provinsi</label>
+                <input name="region" type="text" class="form-control" required>
+                <small class="form-text text-muted">Lokasi provinsi gunung api.</small>
+            </div>
+
+            <button class="btn btn-primary" type="submit">Simpan data</button>
+        </form>
+    </div>
+</div>
+
 @endsection
